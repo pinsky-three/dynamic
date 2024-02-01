042b27e426c2563ca39385ea55e5bce679df050d
Bregy Malpartida <bregy@minsky.cc>
2024-01-31 23:57:54 -0500

    feat: update egui import in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use dynamic::extrusion::process_video;
 use egui_extras::install_image_loaders;
 
-use eframe::egui;
+use eframe::egui::{self};
 
 fn main() -> Result<(), ()> {
     let options = eframe::NativeOptions {
@@ -29,7 +29,7 @@ struct MyApp {
 impl Default for MyApp {
     fn default() -> Self {
         Self {
-            ratio: 990,
+            ratio: 9950,
             sum_filepath: "file://sum.png".to_string(),
         }
     }
@@ -38,33 +38,36 @@ impl Default for MyApp {
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            println!("Hello World!");
-
             ui.heading("Dynamic");
-            // ui.horizontal(|ui| {
-            //     let name_label = ui.label("Your name: ");
-            //     ui.text_edit_singleline(&mut self.name)
-            //         .labelled_by(name_label.id);
-            // });
-            ui.add(egui::Slider::new(&mut self.ratio, 0..=1000).text("ratio"));
+
+            ui.add(
+                egui::Slider::new(&mut self.ratio, 0..=10000)
+                    .step_by(1.)
+                    .text("ratio"),
+            );
+
             if ui.button("Calculate").clicked() {
-                let alpha: f64 = self.ratio as f64 / 1000.;
+                let alpha: f64 = self.ratio as f64 / 10000.;
 
                 process_video("minimal_horse.mp4".to_string(), alpha, 1. - alpha).unwrap();
                 println!("processed video");
+                ctx.request_repaint();
             }
+
             ui.label(format!(
-                "alpha: {:.3}, beta: {:.3}",
-                self.ratio as f64 / 1000.,
-                1. - (self.ratio as f64 / 1000.)
+                "alpha: {:.4}, beta: {:.4}",
+                self.ratio as f64 / 10000.,
+                1. - (self.ratio as f64 / 10000.)
             ));
 
-            // ui.image(egui::include_image!(
-            //     "../../../crates/egui/assets/ferris.png"
-            // ));
+            let rect = ui.available_rect_before_wrap();
+            let available_size = rect.size();
 
-            // ui.image(&self.sum_filepath);
-            ui.add(egui::Image::from_uri(&self.sum_filepath).shrink_to_fit());
+            let image = egui::Image::new(&self.sum_filepath).rounding(3.0);
+
+            image.load_for_size(ctx, available_size).unwrap();
+
+            image.paint_at(ui, rect);
         });
     }
 }
